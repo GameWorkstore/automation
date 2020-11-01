d20b78669ce37ed92b764ccd5b9ef6d255d4828a
HanzaRu <john.rosa@aquiris.com.br>
2020-11-01 11:24:17 -0300

    +Package Updater

---
+++ Editor/PackageUpdater.cs
@@ -0,0 +1,16 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.PackageManager;
+
+namespace GameWorkstore.Automation
+{
+    public class PackageUpdater
+    {
+        [MenuItem("Help/PackageUpdate/GameWorkstore.Automation")]
+        public static void TrackPackages()
+        {
+            Client.Add("git://github.com/GameWorkstore/automation.git");
+        }
+    }
+}
+#endif
